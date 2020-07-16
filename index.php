8213f5c6b484e4e5502c3ca800edd377064d1488
ReinVelt <rein@velt.org>
2020-07-16 13:34:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+	<header>
+		<title>Hackerspace Drenthe (in oprichting)</title>
+		<style>
+			body, a 	{ color:silver; }
+			body 		{ background:black; font-size:large; font-family: "VT323", "Lucida Console", monospace;  margin:5%;  margin-left:15%; margin-right:15%}
+			.hl 		{ color: white;} 
+			h2,h3,h4	{ margin-top:2em;} 
+		</style>
+	</header>
+	<body>
+		<h1><span class="hl">Hackerspace Drenthe</span> (in oprichting)</h1>
+		<p>
+			Welkom bij de website van de hackerspace Drenthe. <br>
+			Onze hackerspace is op dit moment (juli 2020) in oprichting. <br>
+		</p>
+		
+		<h2>Waarom een hackerspace in Drenthe?</h2>
+		<p>
+			Als creatieve techneuten hebben wij de behoefte om elkaar regelmatig te zien, idee&euml;n uit te wisselen en van elkaar te leren.<br>
+			Vanuit Emmen zijn de dichtsbijzijnde hackerspaces in Enschede, Zwolle, Groningen en Lingen (D).<br> 
+			Dat is voor de Drentse techno-hobbyist net iets te ver rijden om regelmatig langs te gaan.<br>
+			Daarom zijn we nu bezig om een hackerspace in de regio van Emmen te starten van/voor/door Drentse techneuten. 
+		</p>
+
+		<h2>Huidige stand van zaken (1 juli 2020)</h2>
+		<p>
+			Op 1 juni 2020 zijn we voor het eerst bij elkaar geweest om te bepalen hoe we gaan starten. Daaruit zijn een paar aktiepunten gekomen.
+			<ul>
+				<li>mensen betrekken bij ons plan (prio 1)</li>
+				<li>kernteam regelen van 5-7 personen (prio 2)</li>
+				<li>website / wiki maken</li>
+				<li>mailinglijst maken</li>
+				<li>irc kanaal maken  [DONE]-> #hackerspace-drenthe (<a href="https://webchat.oftc.net/">irc.oftc.net</a>)</li>
+				<li>BBQ organiseren</li>
+				<li>Locatie zoeken</li>
+			</ul>
+		</p>
+
+		<h2>Hulp gevraagd</h2>
+		<p>
+			We zijn nog maar net begonnen en er is nog veel te doen. Daarom zijn we op zoek naar mensen die ons hierbij willen helpen.<br>
+			Lijkt het je leuk om mee te bouwen en deel te nemen aan onze hackerspace neem dan contact op.<br>
+			Iedereen is welkom.
+		</p>
+	
+		<h2>Contact informatie</h2>
+		<p>
+			Postadres:<br>
+			Oosterdiep OZ 96<br>
+			7881 CV  Emmer-Compascuum<br>
+			+31 (0)6 376 07 455<br>
+			<a href="mailto:bestuur@hackerspace-drenthe.nl" title="stuur email naar bestuur@hackerspace-drenthe.nl">bestuur at hackerspace-drenthe.nl</a><br>
+		</p>
+
+		<h2>Links</h2>
+		<ul>
+			<li><a target="new" href="http://hackerspaces.nl" title="link naar website met nederlandse hackerspaces">Nederlandse hackerspaces</a></li>
+			<li><a target="new" href="https://hackerspace.design/" title="Hackerspace blueprint">Hackerspace blueprint</a></li>
+		</ul>
+		 
+	</body>
+</html>
